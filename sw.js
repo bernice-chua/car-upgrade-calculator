135d9811e787ec7db8efc4789a4137ab2cac4a47
bernice-chua <chxian@gmail.com>
2026-09-16 00:01:02 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='car-upgrade-v9';
-const STATIC=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./icons/logo.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{const r=e.request;if(r.mode==='navigate'){e.respondWith(fetch(r).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put('./index.html',cp));return resp}).catch(()=>caches.match('./index.html')));return}e.respondWith(caches.match(r).then(hit=>hit||fetch(r).then(resp=>{if(r.method==='GET'&&resp.ok){const cp=resp.clone();caches.open(CACHE).then(c=>c.put(r,cp))}return resp})))});
+const CACHE='car-upgrade-v10';
+const STATIC=['./','./index.html','./manifest.webmanifest','./manifest.json','./icon-192.png','./icon-512.png','./maskable-512.png','./logo.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{const request=event.request;if(request.method!=='GET')return;if(request.mode==='navigate'){event.respondWith(fetch(request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return response}).catch(()=>caches.match('./index.html')));return}event.respondWith(caches.match(request).then(cached=>cached||fetch(request).then(response=>{if(response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(request,copy))}return response})))});
