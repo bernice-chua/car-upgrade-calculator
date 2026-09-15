02f7ab1200aea86673568a87d016c96c494b8eb8
bernice-chua <chxian@gmail.com>
2026-09-15 23:27:06 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='car-upgrade-v8';
+const CACHE='car-upgrade-v9';
 const STATIC=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./icons/logo.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
